d7db2a443d22c1d2618218a50f11f796d4636639
José Romildo Malaquias <malaquias@gmail.com>
2019-03-20 23:52:38 -0300

    Add shell.nix for use with NixOS

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ nixpkgs ? import <nixpkgs> {} } :
+
+let
+  inherit (nixpkgs) pkgs;
+  ocamlPackages = pkgs.ocamlPackages_latest;
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "my-ocaml-env-0";
+  buildInputs = [
+    ocamlPackages.ocaml
+    ocamlPackages.ocamlbuild
+    ocamlPackages.findlib
+    ocamlPackages.camomile
+    ocamlPackages.merlin
+    ocamlPackages.menhir
+    pkgs.dune
+    pkgs.emacs
+  ];
+}
