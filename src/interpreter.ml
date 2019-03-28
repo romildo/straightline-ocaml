a938bbd769fd4ebaeff5c3ada96e00a65647fc68
José Romildo Malaquias <malaquias@gmail.com>
2019-03-27 21:06:18 -0300

    Implement interpreter

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,3 +1,31 @@
+open Absyn
+
+type memory = (id, float) Hashtbl.t
+
+let rec run m prog =
+  match prog with
+  | CompoundStm (s1, s2) -> run m s1; run m s2
+  | AssignStm (v, e) -> Hashtbl.replace m v (eval m e)
+  | PrintStm list -> List.iter
+                       (fun e -> print_float (eval m e); print_char ' ')
+                       list;
+                     print_newline ()
+and eval m exp =
+  match exp with
+  | NumExp cte -> cte
+  | IdExp v -> ( try Hashtbl.find m v
+                 with Not_found -> 0.0
+               )
+  | OpExp (e1, op, e2) -> let v1 = eval m e1 in
+                          let v2 = eval m e2 in
+                          ( match op with
+                            | Plus -> v1 +. v2
+                            | Minus -> v1 -. v2
+                            | Times -> v1 *. v2
+                            | Div -> v1 /. v2
+                          )
+  | EseqExp (s, e) -> run m s; eval m e
 
 let interpret prog =
-  ()
+  let m = Hashtbl.create 0 in
+  run m prog
