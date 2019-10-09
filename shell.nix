c1ddd708243445d33cbf78b6d99c30056ee25684
José Romildo Malaquias <malaquias@gmail.com>
2019-10-09 11:49:21 -0300

    - Use ocaml 4.07 - Remove ocamlbuild

--- shell.nix
+++ shell.nix
@@ -2,14 +2,15 @@
 
 let
   inherit (nixpkgs) pkgs;
-  ocamlPackages = pkgs.ocamlPackages_latest;
+  #ocamlPackages = pkgs.ocamlPackages;
+  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_07;
+  #ocamlPackages = pkgs.ocamlPackages_latest;
 in
 
 pkgs.stdenv.mkDerivation {
   name = "my-ocaml-env-0";
   buildInputs = [
     ocamlPackages.ocaml
-    ocamlPackages.ocamlbuild
     ocamlPackages.findlib
     ocamlPackages.camomile
     ocamlPackages.merlin
