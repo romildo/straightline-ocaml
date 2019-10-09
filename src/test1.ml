d4cbaabc27d5dcf07f5b2d902c2755323619d3b4
José Romildo Malaquias <malaquias@gmail.com>
2019-10-09 11:52:12 -0300

    augment test1

--- src/test1.ml
+++ src/test1.ml
@@ -1,10 +1,31 @@
 (*
+  altura := 1.73
+ *)
+
+let prog1 = AssignStm ("altura", NumExp 1.73)
+
+(*
+  print(43, 7, 0)
+ *)
+
+let prog2 = PrintStm [NumExp 43; NumExp 7; NumExp 0]
+
+(*
+  x := 2 + 3;
+  print(x)
+ *)
+
+let prog3 =
+  CompoundStm (AssignStm ("x", OpExp (NumExp 2, Plus, NumExp 3)),
+               PrintStm [IdExp "x"])
+
+(*
   x := 2 + 3 * 4;
   print(x)
 
  *)
 
-let prog1 =
+let prog4 =
   CompoundStm (AssignStm ("x",
                           OpExp (NumExp 2,
                                  Plus,
@@ -12,3 +33,9 @@ let prog1 =
                                         Times,
                                         NumExp 4))),
                PrintStm [IdExp "x"])
+
+(*
+  a := 5 + 3;
+  b := (print(a, a-1), 10*a);
+  print(b)
+ *)
